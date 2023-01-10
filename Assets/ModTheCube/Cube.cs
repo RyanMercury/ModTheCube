d0727ab11ee6aca2b3aadb22b336c7d1bf0c3bba
Ryan Turner <ryan.turner1@hotmail.com>
2023-01-10 15:30:39 +0000

    Mod The Cube(Unity/C#)
    Mod The Cube exercise made with Unity and C#

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    public float AngleX;
+    public float cubeScale = 5f;
+    public float angleY;
+    public float anglez;
+
+   
+
+    
+    void Start()
+    {
+        movingPos();
+        InvokeRepeating("colourChange",5,5);
+        InvokeRepeating("movingPos", 5, 5);
+
+    }
+
+    void colourChange()
+    {
+
+    float red = Random.value;
+    float green = Random.Range(0, 1f);
+    float blue = Random.Range(0, 1f);
+    float alpha = Random.Range(0, 1f);
+        Material material = Renderer.material;
+        material.color = new Color(red, green, blue, alpha);
+    }
+
+  void movingPos()
+    {
+        {
+            float xPos = Random.Range(0, 6);
+            float yPos = Random.Range(0, 6);
+            float zPos = Random.Range(0, 6);
+            transform.position = new Vector3(xPos, yPos, zPos);
+
+            
+        }
+    }
+    void spinAngles()
+    {
+        AngleX = Random.Range(0, 5);
+        angleY = Random.Range(0, 5);
+        anglez = Random.Range(0, 7);
+        transform.Rotate(AngleX * Time.deltaTime, angleY * Time.deltaTime, anglez * Time.deltaTime);
+    }
+    void Update()
+    {
+       // colourChange();
+       // InvokeRepeating("movingPos", 5, 5);
+         spinAngles();
+            transform.localScale = Vector3.one * cubeScale;
+
+    
+    
+            
+
+    }
+  
+}
+
